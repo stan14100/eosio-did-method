67cd38a9910ff3cddf7461adb420219b8c7264f0
Andreas Kouloumos <kouloumosa@gmail.com>
2020-06-01 11:58:45 +0300

    update smart-contract

--- registry-contract/contracts/registry/src/registry.cpp
+++ registry-contract/contracts/registry/src/registry.cpp
@@ -1,28 +1,22 @@
 #include <registry.hpp>
 
-ACTION registry::registerdid(name owner, checksum256 ddoHash) {
+ACTION registry::updatedid(name owner, checksum256 ddoHash) {
     require_auth(owner);
 
     records_index records_table(get_self(), owner.value );
-    
     auto records_itr = records_table.find(owner.value);
-    check( records_itr == records_table.end(), "A DID has been registered for this account");
-    records_table.emplace( get_self(), [&]( auto& record ){
-        record.owner = owner;
-        record.ddoHash = ddoHash;
+    //No DID registered for this account -> New DID is registered
+    if (records_itr == records_table.end()) {
+        records_table.emplace( get_self(), [&]( auto& record ){
+            record.owner = owner;
+            record.ddoHash = ddoHash;
+        });
+    } 
+    // DID has been registered for this account -> Updating DID
+    else {
+        records_table.modify(records_itr, get_self(), [&]( auto& record ){
+            record.owner = owner;
+            record.ddoHash = ddoHash;
     });
-}
-
-
-ACTION registry::updatedid(name owner, checksum256 new_ddoHash) {
-    require_auth(owner);
-
-    records_index records_table(get_self(), owner.value );
-
-    auto records_itr = records_table.find(owner.value);
-    check( records_itr != records_table.end(), "No DID has been registered for this account");
-    records_table.modify(records_itr, get_self(), [&]( auto& record ){
-        record.owner = owner;
-        record.ddoHash = new_ddoHash;
-    });
+    }    
 }
